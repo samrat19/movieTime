3ea017e84f99397fa23406ecfe08417872dbbe6b
ATRI DAS <babinsamrat123@gmail.com>
2019-01-03 17:45:16 +0530

    ugradation

--- lib/detailspage.dart
+++ lib/detailspage.dart
@@ -17,11 +17,13 @@ class _DetailsPageState extends State<DetailsPage> {
   final String poster;
   final String story;
   final String title;
+
   _DetailsPageState(this.poster,this.story,this.title);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo,
       appBar: AppBar(
         title: Text(this.title),
       ),
@@ -32,20 +34,24 @@ class _DetailsPageState extends State<DetailsPage> {
               child: Image.network(this.poster),
             ),
             Container(
+              height: 30.0,
+              color: Colors.indigo,
+            ),
+            Container(
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
+                borderRadius: BorderRadius.circular(5.0),
                 clipBehavior: Clip.hardEdge,
                 child: Container(
-                    color: Colors.purpleAccent,
-                    height: 175.0,
+                  height: 300.0,
+                    color: Colors.indigo,
                     child: ListView(
                       children: <Widget>[
-                         Text(this.story,style: TextStyle(fontSize: 25.0,color: Colors.white),),
+                         Text(this.story,style: TextStyle(fontSize: 20.0,color: Colors.white),),
                       ],
                     ),
                 )
               )
-            )
+            ),
           ],
         )
       ),
