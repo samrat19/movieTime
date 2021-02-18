5f90264076625c59c50bdc9b1f555351f50a0c26
Atri Das <babinsamrat123@gmail.com>
2021-02-18 11:17:21 +0530

    removes some code chunks

--- lib/logic/model/movie_cast_model.dart
+++ lib/logic/model/movie_cast_model.dart
@@ -1,9 +1,8 @@
 class MovieCastModel {
   int id;
   List<Cast> cast;
-  List<Crew> crew;
 
-  MovieCastModel({this.id, this.cast, this.crew});
+  MovieCastModel({this.id, this.cast});
 
   MovieCastModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -13,24 +12,6 @@ class MovieCastModel {
         cast.add(new Cast.fromJson(v));
       });
     }
-    if (json['crew'] != null) {
-      crew = new List<Crew>();
-      json['crew'].forEach((v) {
-        crew.add(new Crew.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    if (this.cast != null) {
-      data['cast'] = this.cast.map((v) => v.toJson()).toList();
-    }
-    if (this.crew != null) {
-      data['crew'] = this.crew.map((v) => v.toJson()).toList();
-    }
-    return data;
   }
 }
 
@@ -75,79 +56,5 @@ class Cast {
     character = json['character'];
     creditId = json['credit_id'];
     order = json['order'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['adult'] = this.adult;
-    data['gender'] = this.gender;
-    data['id'] = this.id;
-    data['known_for_department'] = this.knownForDepartment;
-    data['name'] = this.name;
-    data['original_name'] = this.originalName;
-    data['popularity'] = this.popularity;
-    data['profile_path'] = this.profilePath;
-    data['cast_id'] = this.castId;
-    data['character'] = this.character;
-    data['credit_id'] = this.creditId;
-    data['order'] = this.order;
-    return data;
-  }
-}
-
-class Crew {
-  bool adult;
-  int gender;
-  int id;
-  String knownForDepartment;
-  String name;
-  String originalName;
-  double popularity;
-  String profilePath;
-  String creditId;
-  String department;
-  String job;
-
-  Crew(
-      {this.adult,
-        this.gender,
-        this.id,
-        this.knownForDepartment,
-        this.name,
-        this.originalName,
-        this.popularity,
-        this.profilePath,
-        this.creditId,
-        this.department,
-        this.job});
-
-  Crew.fromJson(Map<String, dynamic> json) {
-    adult = json['adult'];
-    gender = json['gender'];
-    id = json['id'];
-    knownForDepartment = json['known_for_department'];
-    name = json['name'];
-    originalName = json['original_name'];
-    popularity = json['popularity'];
-    profilePath = json['profile_path'];
-    creditId = json['credit_id'];
-    department = json['department'];
-    job = json['job'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['adult'] = this.adult;
-    data['gender'] = this.gender;
-    data['id'] = this.id;
-    data['known_for_department'] = this.knownForDepartment;
-    data['name'] = this.name;
-    data['original_name'] = this.originalName;
-    data['popularity'] = this.popularity;
-    data['profile_path'] = this.profilePath;
-    data['credit_id'] = this.creditId;
-    data['department'] = this.department;
-    data['job'] = this.job;
-    return data;
   }
 }
